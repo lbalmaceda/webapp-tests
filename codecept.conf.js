1f562c46853f2952c1d3104b3f8a8374255c0c21
Luciano Balmaceda <balmacedaluciano@gmail.com>
2018-03-23 14:13:43 -0300

    use dynamic config and allow multiple browsers

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,10 +1,15 @@
-{
+exports.config = {
   "tests": "./*_test.js",
   "timeout": 10000,
   "output": "out",
+  "multiple": {
+    "basic": {
+      "browsers": ["chrome", "firefox"]
+    }
+  },
   "helpers": {
-    "Puppeteer": {
-      "url": "http://localhost:3000",
+    "Puppeteer": {  
+      "url": process.env.WEBAPP_URL || "http://localhost:3000",
       "chrome":{
         "args": ["--no-sandbox", "--disable-setuid-sandbox"]
       }
--- lock_login_test.js
+++ lock_login_test.js
@@ -5,8 +5,9 @@ Scenario('Log in using Lock', (I) => {
     I.waitForVisible('#qsLoginBtn', 3);
     I.click('#qsLoginBtn');
     I.waitForVisible('input[name="password"]', 5);
+    I.wait(1);
     I.fillField('input[name="username"]', 'asdasd');
     I.fillField('input[name="password"]', 'asdasd');
     I.click('.auth0-lock-widget-container .auth0-lock-submit');
-    I.waitForVisible('#qsLogoutBtn', 3);
+    I.retry(2).waitForVisible('#qsLogoutBtn', 5);
 });
